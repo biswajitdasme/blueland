b5aa07f519e268195259a7542dc8e176c143af93
Biswajit Das <bd.partha@gmail.com>
2022-02-06 21:16:40 +0600

    Add to cart button added to product card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,4 @@
-import {Image, Link} from '@shopify/hydrogen';
+import {Image, Link, ProductProvider, AddToCartButton} from '@shopify/hydrogen';
 
 import MoneyCompareAtPrice from './MoneyCompareAtPrice.client';
 import MoneyPrice from './MoneyPrice.client';
@@ -15,9 +15,9 @@ export default function ProductCard({product}) {
   }
 
   return (
-    <div className="text-md mb-4 relative">
-      <Link to={`/products/${product.handle}`}>
-        <div className="rounded-lg border-2 border-gray-200 mb-2 relative flex items-center justify-center overflow-hidden object-cover h-96">
+    <ProductProvider product={product}>
+      <div className="text-md mb-4 relative">
+        <div className="rounded-lg border-2 border-gray-200 mb-2 relative flex items-center justify-center overflow-hidden object-cover h-96 p-2">
           {selectedVariant.image ? (
             <>
               <Image
@@ -31,17 +31,25 @@ export default function ProductCard({product}) {
               Out of stock
             </div>
           )}
+          <AddToCartButton
+            variantId={selectedVariant.id}
+            className="inline-block absolute bottom-2 bg-blue-500 hover:bg-gradient-to-r hover:from-blue-200 hover:via-purple-200 hover:to-orange-200 hover:text-gray-700  border-2 border-transparent
+              hover:border-gray-500 rounded-full text-xl w-full my-2 text-white text-center mx-4 p-3 capitalize font-semibold transition-all duration-100"
+          >
+            Quick Add | ${selectedVariant.priceV2.amount}
+          </AddToCartButton>
         </div>
         <div className="flex justify-between items-center">
-          <span className="text-black font-semibold mb-0.5">
-            {product.title}
-          </span>
-
+          <Link to={`/products/${product.handle}`}>
+            <span className="text-black font-semibold mb-0.5">
+              {product.title}
+            </span>
+          </Link>
           {/* {product.vendor && (
-          <p className="text-gray-900 font-medium text-sm mb-0.5">
-            {product.vendor}
-          </p>
-          )} */}
+              <p className="text-gray-900 font-medium text-sm mb-0.5">
+              {product.vendor}
+              </p>
+              )} */}
 
           <div className="flex ">
             {selectedVariant.compareAtPriceV2 && (
@@ -50,7 +58,7 @@ export default function ProductCard({product}) {
             <MoneyPrice money={selectedVariant.priceV2} />
           </div>
         </div>
-      </Link>
-    </div>
+      </div>
+    </ProductProvider>
   );
 }
